4b8b83948e1cf274b645bcc45ea1d02156d5554e
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-10-10 18:45:44 +0000

    Created a household app and gave it a model

---
+++ backend/households/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils.text import slugify
+
+class Household(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from households.models import Household
 
 # A model for the users profile
 
@@ -11,10 +12,10 @@ class Profile(models.Model):
     ]
     member = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
-    household = models.CharField(max_length=30)
+    household = models.ForeignKey(Household, on_delete=models.CASCADE, related_name='members')
 
     def __str__(self):
-        return f"{self.member.username} is a {self.role} in the {self.household} household"
+        return f"{self.member.username} is a {self.role} in the {self.household.name} household"
 
 
 def create_profile(sender, instance, created, **kwargs):
