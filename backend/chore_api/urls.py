7a36a13b1c3963104a402a227ff6e49a8e297a37
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-10-11 20:52:55 +0000

    Added rest-framework log in and out functionality for developing purposes

--- backend/chore_api/urls.py
+++ backend/chore_api/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
     path('', include('households.urls')),
 ]
