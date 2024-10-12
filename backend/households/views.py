12e6109641b6014b15307e39158b139a3a7bb026
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-10-12 10:19:41 +0000

    Add searchfields and filterset fields to the household view

--- backend/households/views.py
+++ backend/households/views.py
@@ -1,7 +1,20 @@
-from rest_framework import generics
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Household
 from .serializers import HouseholdSerializer
+from chore_api.permissions import IsOwnerOrReadOnly
 
-class HouseholdList(generics.ListCreateAPIView):
-    queryset = Household.objects.all()
+class HouseholdList(generics.ListAPIView):
     serializer_class = HouseholdSerializer
+    queryset = Household.objects.all()
+    permission_classes = [IsOwnerOrReadOnly]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter
+    ]
+    filterset_fields = [
+        'name', 'members__member__username'
+    ]
+    search_fields = [
+        'name', 'members__member__username'
+    ]
