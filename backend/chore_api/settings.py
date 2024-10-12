2cb9985f1f14dd597d1b9cd1e89cc3c6c1af3e77
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-10-12 11:11:51 +0000

    Created a model for tasks

--- backend/chore_api/settings.py
+++ backend/chore_api/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
     'profiles',
     'households',
+    'tasks',
 ]
 
 MIDDLEWARE = [
--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from profiles.models import Profile
 
-# Create your models here.
+# A model for the tasks
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+    ]
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    assigned_to = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return self.title
+
