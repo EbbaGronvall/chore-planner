5091c40034e99c4f59dc5a218bde56c67a302f70
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-10-16 16:47:37 +0000

    imported api_view and response to view

--- chore_api/views.py
+++ chore_api/views.py
@@ -1,8 +1,17 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .settings import (
     JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
     JWT_AUTH_SECURE
 )
 
+
+@api_view()
+def root_route(request):
+    return Response({
+        "message": "Welcome to my drf API!"
+    })
+    
 # dj-rest-auth logout view fix
 @api_view(['POST'])
 def logout_route(request):
