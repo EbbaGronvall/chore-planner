7bf5427d3a9912968c51728ffd5cd62f98022f2c
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-10-16 20:38:55 +0000

    Navlinks set up and styling added to them

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,27 @@
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import { Container } from "react-bootstrap";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    <div className={styles.App}>
-      <NavBar />
-    </div>
-  );
+	return (
+		<div className={styles.App}>
+			<NavBar />
+			<Container className={styles.Main}>
+				<Switch>
+					<Route exact path="/" render={() => <h1>Your Home</h1>} />
+					<Route
+						exact
+						path="/noticeboard"
+						render={() => <h1>Noticeboard</h1>}
+					/>
+					<Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+					<Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route render={() =><p>Page not found!</p>} />
+				</Switch>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,18 +1,47 @@
 import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap"
-import styles from "../styles/NavBar.module.css"
+import { Navbar, Container, Nav } from "react-bootstrap";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
 	return (
-		<Navbar className={styles.NavBar} expand="md" fixed="top">
+		<Navbar variant="light" className={styles.NavBar} expand="md" fixed="top">
 			<Container>
-				<Navbar.Brand className={styles.Brand}>ChorePlanner</Navbar.Brand>
+				<NavLink to="/">
+					<Navbar.Brand className={styles.Brand}>ChorePlanner</Navbar.Brand>
+				</NavLink>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ml-auto text-left">
-						<Nav.Link>Noticeboard <i class="fas fa-thin fa-thumbtack"></i></Nav.Link>
-						<Nav.Link>Sign in <i class="fas fa-solid fa-arrow-right-to-bracket"></i></Nav.Link>
-						<Nav.Link>Sign up <i class="fa-solid fa-user-plus"></i></Nav.Link>
+						<NavLink
+							exact
+							to="/"
+							className={styles.NavLink}
+							activeClassName={styles.Active}
+						>
+							Home <i class="fa-solid fa-house-chimney"></i>
+						</NavLink>
+						<NavLink
+							to="/noticeboard"
+							className={styles.NavLink}
+							activeClassName={styles.Active}
+						>
+							Noticeboard <i class="fas fa-thin fa-thumbtack"></i>
+						</NavLink>
+						<NavLink
+							to="/signin"
+							className={styles.NavLink}
+							activeClassName={styles.Active}
+						>
+							Sign in <i class="fas fa-solid fa-arrow-right-to-bracket"></i>
+						</NavLink>
+						<NavLink
+							to="/signup"
+							className={styles.NavLink}
+							activeClassName={styles.Active}
+						>
+							Sign up <i class="fa-solid fa-user-plus"></i>
+						</NavLink>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
