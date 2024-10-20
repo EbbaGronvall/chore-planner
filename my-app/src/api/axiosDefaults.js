e79fed09837033c7502492b06ebd87a096305c94
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-10-20 19:47:14 +0000

    Frontend removed to make this a pure API

--- my-app/src/api/axiosDefaults.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = 'https://chore-planner-b346832e027b.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
